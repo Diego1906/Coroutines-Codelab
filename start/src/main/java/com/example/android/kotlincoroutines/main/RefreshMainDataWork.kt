23ac65d290675cb6ad3c5f90bae549d92393be67
Diego <di_fsantos@yahoo.com.br>
2020-01-16 15:26:01 -0300

    Adicionada uma função de order superior ou High Order Function

--- start/src/main/java/com/example/android/kotlincoroutines/main/RefreshMainDataWork.kt
+++ start/src/main/java/com/example/android/kotlincoroutines/main/RefreshMainDataWork.kt
@@ -36,13 +36,20 @@ class RefreshMainDataWork(context: Context, params: WorkerParameters, private va
      * start just enough to run this [Worker].
      */
     override suspend fun doWork(): Result {
-        return Result.success()         // TODO: Use coroutines from WorkManager
+        val dataBase = getDatabase(applicationContext)
+        val repository = TitleRepository(network, dataBase.titleDao)
+
+        return try {
+            repository.refreshTitle()
+            Result.success()
+        } catch (error: TitleRefreshError) {
+            Result.failure()
+        }
     }
 
     class Factory(val network: MainNetwork = getNetworkService()) : WorkerFactory() {
         override fun createWorker(appContext: Context, workerClassName: String, workerParameters: WorkerParameters): ListenableWorker? {
             return RefreshMainDataWork(appContext, workerParameters, network)
         }
-
     }
 }
